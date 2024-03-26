a91140b90d2c5cd72decc11aa0eff636743e6604
DeanNguyen233 <162050804+DeanNguyen233@users.noreply.github.com>
2024-03-26 17:31:15 +0800

    Add files via upload

--- Arithmetic Formater.py
+++ Arithmetic Formater.py
@@ -47,15 +47,18 @@ def arithmetic_arranger(problems, show_answer=False):
                 result = str(int(first_number) + int(second_number))
             elif operator == '-':
                 result = str(int(first_number) - int(second_number))
-            row4 += result.rjust(5) + '    '
+            row4 += result.rjust(max_length + 2) + '    '
         if show_answer == False:
             row4 = ''
     
-    # Combine the rows
-    sorting_problem = row1.rstrip() + '\n' + row2.rstrip() + '\n' + row3.rstrip() + '\n' + row4.rstrip()
+    # Check different format if show_answer is True
+    if show_answer == True:
+        sorting_problem = row1.rstrip() + '\n' + row2.rstrip() + '\n' + row3.rstrip() + '\n' + row4.rstrip()
+    if show_answer == False:
+        sorting_problem = row1.rstrip() + '\n' + row2.rstrip() + '\n' + row3.rstrip() + row4.rstrip()
     
     # Return the arranged problems
     return sorting_problem
 
-print(arithmetic_arranger(["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"]))
-#print(str("  11      3801      1      123         1\n+  4    - 2999    + 2    +  49    - 9380\n----    ------    ---    -----    ------."))
+print(str(arithmetic_arranger(["32 - 698", "1 - 3801", "45 + 43", "123 + 49", "988 + 40"], True)))
+print(str("   32         1      45      123      988\n- 698    - 3801    + 43    +  49    +  40\n-----    ------    ----    -----    -----\n -666     -3800      88      172     1028"))
