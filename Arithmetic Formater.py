9119e50014bee231da00928264f66363c2533e96
DeanNguyen233 <162050804+DeanNguyen233@users.noreply.github.com>
2024-03-26 09:37:04 +0800

    Finished and uploaded the final file
    Working as intended

--- Arithmetic Formater.py
+++ Arithmetic Formater.py
@@ -5,49 +5,53 @@ def arithmetic_arranger(problems, show_answer=False):
     row3 = ''
     row4 = ''
     
-    # Sorting out all of the problem, seperate in between (string1, operator, string2) in 1 problem.
+    if len(problems) > 5:
+        return "Error: Too many problems."
+    
     for problem in problems:
         if '+' in problem:
             operator_index = problem.find('+')
-            first_number = problem[:operator_index]
-            second_number = problem[operator_index + 1:]
+            first_number = problem[:operator_index].strip()
+            second_number = problem[operator_index + 1:].strip()
             operator = '+'
-            if first_number != range(-9999,9999) or second_number != range(-9999,9999):
+            if not first_number.isdigit() or not second_number.isdigit():
+                return "Error: Numbers must only contain digits."
+            if len(first_number) > 4 or len(second_number) > 4:
                 return "Error: Numbers cannot be more than four digits."
         elif '-' in problem:
             operator_index = problem.find('-')
-            first_number = problem[:operator_index]
-            second_number = problem[operator_index + 1:]
+            first_number = problem[:operator_index].strip()
+            second_number = problem[operator_index + 1:].strip()
             operator = '-'
-            # This is to confirm the number can only be 4 digit maximum
-            if first_number != range(-9999,9999) or second_number != range(-9999,9999):
+            if not first_number.isdigit() or not second_number.isdigit():
+                return "Error: Numbers must only contain digits."
+            if len(first_number) > 4 or len(second_number) > 4:
                 return "Error: Numbers cannot be more than four digits."
         else:
             return "Error: Operator must be '+' or '-'."
         
+        # Construct the sorted problem string
         sorting_problem += f"{first_number.strip()} {operator} {second_number.strip()}\n"
-            #return "Error: Numbers must only contain digits."
 
-    # Drawing out rows and input the number on that "rows"
-    max_row_character = max(len(first_number), len(second_number))
-    if max_row_character <= 4:
-        row1 += first_number.rjust(max_row_character + 2) + '    '
-        row2 += operator.ljust(max_row_character) + second_number.rjust(max_row_character + 1) + '    '
-        row3 += '-'*(max_row_character + 2) + '    '
+        # Sorting the formats
+        row1 += first_number.rjust(5) + '    '
+        row2 += operator + second_number.rjust(4) + '    '
+        row3 += '-' * 5 + '    '
 
-        # Calculate the result of the Arithmetic
+        # Calculate the result of the arithmetic if show_answer is True
         if show_answer == True:
             if operator == '+':
                 result = str(int(first_number) + int(second_number))
             elif operator == '-':
                 result = str(int(first_number) - int(second_number))
-            else:
-                return "Error: Operator must be '+' or '-'."
-            row4 += result.rjust(max_row_character + 2) + '    '
-    else:
-        return "Error: Too many problems."
+            row4 += result.rjust(5) + '    '
+        if show_answer == False:
+            row4 = ''
+    
+    # Combine the rows
+    sorting_problem = '\n'.join([row1.rstrip(), row2.rstrip(), row3.rstrip(), row4.rstrip()])
+    
+    # Return the arranged problems
     return sorting_problem
 
-print(arithmetic_arranger(["32+698", "3801-2", "45+43", "123 +49"]))
-
-
+print(arithmetic_arranger(["32-698", "3801-2", "45+43", "123 +49", "32-698"], show_answer=False))
